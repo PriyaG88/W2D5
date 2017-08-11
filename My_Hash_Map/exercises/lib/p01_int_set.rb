dc8e4219ea419732a1fe94518fdc2c79e547ed49
App Academy Student <student@appacademy.io>
2017-08-11 13:27:33 -0700

    secret commit

---
+++ My_Hash_Map/exercises/lib/p01_int_set.rb
@@ -0,0 +1,106 @@
+class MaxIntSet
+  def initialize(max)
+    @max = (0..max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    raise "Out of bounds" if !is_valid?(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    @store[num] = false if include?(num)
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    @max.include?(num)
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    if !include?(num)
+      self[num] << num
+    end
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    idx = num % 20
+    @store[idx]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    resize! if @count == num_buckets
+
+    if !include?(num)
+      self[num] << num
+      @count += 1
+    end
+  end
+
+  def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+
+
+  end
+end
